903aee0b1c07575b62b2f552045541e80ac14f2c
hafdon <zweisser@gmail.com>
2024-09-30 06:44:05 -0700

    Initial Commit

---
+++ IrishPronounQuiz/IrishPronounQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrishPronounQuizApp.swift
+//  IrishPronounQuiz
+//
+//  Created by starkat on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct IrishPronounQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
